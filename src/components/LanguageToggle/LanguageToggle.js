c0f982dbcbf463d29195f409e283cd1bcf53e6b6
Yaiza Vallejo Caro <yaizavallejo16@gmail.com>
2022-06-04 13:19:11 +0200

    Change toggle language

--- src/components/LanguageToggle/LanguageToggle.js
+++ src/components/LanguageToggle/LanguageToggle.js
@@ -1,18 +1,52 @@
 import { useTranslation } from 'react-i18next';
-import { Box, Button } from '@mui/material';
+
+import { ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { useState } from 'react';
+import ImageEs from '../../assets/images/spain.png';
+import ImageEn from '../../assets/images/united-kingdom.png';
 
 function LanguageToggle() {
+  const [alignment, setAlignment] = useState('left');
   const { i18n } = useTranslation();
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
   return (
-    <Box>
-      <span className="fi fi-gr fis" style={{ width: '20px' }} />
-      <Button sx={{ color: 'white' }} onClick={() => i18n.changeLanguage('en')}>
-        EN
-      </Button>
-      <Button sx={{ color: 'white' }} onClick={() => i18n.changeLanguage('es')}>
-        ES
-      </Button>
-    </Box>
+    <ToggleButtonGroup
+      value={alignment}
+      exclusive
+      onChange={handleAlignment}
+      aria-label="text alignment"
+      color="secondary"
+    >
+      <ToggleButton
+        value="left"
+        aria-label="spain language"
+        onClick={() => i18n.changeLanguage('es')}
+        style={{ width: '50px', height: '50px' }}
+      >
+        <img
+          alt="spain flat"
+          src={ImageEs}
+          style={{ objectFit: 'contain' }}
+          width="35px"
+        />
+      </ToggleButton>
+      <ToggleButton
+        value="right"
+        aria-label="english language"
+        onClick={() => i18n.changeLanguage('en')}
+        style={{ width: '50px', height: '50px' }}
+      >
+        <img
+          alt="united-kingdom-flat"
+          src={ImageEn}
+          style={{ objectFit: 'contain' }}
+          width="35px"
+        />
+      </ToggleButton>
+    </ToggleButtonGroup>
   );
 }
 
